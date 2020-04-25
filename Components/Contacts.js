e24113eef60364158e82d0c61db8b028c1b11247
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-04-25 23:04:46 +0100

    Add popup & fonts in Contacts

--- Components/Contacts.js
+++ Components/Contacts.js
@@ -1,109 +1,168 @@
-// import React from 'react';
-// import { View, Text, StyleSheet, Button } from 'react-native';
-
 
 // const Contacts  = ({ navigation }) => {
 //     return (
 //       <View style={styles.container}><Text>Contacts</Text>
 //       <Button
 //         title="Go to Feed"
-//         onPress={() => navigation.navigate('Feed')}
+// onPress={() => navigation.navigate('Feed')}
 //       />
 //       </View>
 //     );
 // };
 
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1, 
-//     alignItems: 'center', 
-//     justifyContent: 'center',
-//     backgroundColor: '#FFFFFF',
-//   },
-// });
-
-import React, { Component } from "react";
-import { Text, TouchableOpacity, View, StyleSheet, Image, CheckBox } from "react-native";
-import Dialog from "react-native-dialog";
+import React, { Component, useState } from "react";
+import { Text, TouchableOpacity, View, StyleSheet, Image, CheckBox, Modal, TextInput, } from "react-native";
 
 import Feed from './Feed';
 
-const Contacts = (props) => {
-  return (
-    <View style={styles.container}>
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
 
-      {/* THIS IS THE USER INFO */}
-      <View style={{ flexDirection: 'row', padding: 20, justifyContent: 'flex-end', paddingBottom: '0.5%' }}>
-        <View style={{ width: 150, height: 150 }}>
-          <Image style={{ width: '100%', height: '100%' }} source={require('../assets/JohnDoe.png')} />
-        </View>
-      </View>
+const Contacts = ({ navigation }) => {
 
-      <View style={{ alignItems: 'flex-end', justifyContent: 'flex-end', padding: 10, paddingTop: '0.5%' }}>
-        <View style={{ flexWrap: 'wrap', marginRight: '5%' }}>
-          <Text style={{ fontSize: 22 }}>John Doe</Text>
-        </View>
+  //Pop-up
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalTwoOpen, setModalTwoOpen] = useState(false);
 
-        <View style={{ flexWrap: 'wrap', marginRight: '5%' }}>
-          <Text style={{ fontSize: 14 }}>NHS NUMBER: 000 000 0000</Text>
-        </View>
+  // fonts load
+  let [fontsLoaded] = useFonts({
+    'Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+    'Bold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+  });
 
-      </View>
-      {/* THIS IS THE USER INFO */}
+  // check if fonts are loaded
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
 
-      {/* THIS IS THE BUTTON */}
-      <View style={{ padding: '2%' }}>
 
-        <TouchableOpacity style={{
-          marginRight: '15%', marginLeft: '15%', alignItems: 'center', justifyContent: 'center', flexDirection: 'row', backgroundColor: 'white', padding: 10, shadowColor: 'rgba(0,0,0, .25)',
-          shadowOffset: { height: 0, width: 0 }, shadowOpacity: 2, shadowRadius: 4, elevation: 5, borderRadius: 40,
-        }} 
-        // onPress={this.showDialog}
-        onPress={() => { props.navigation.navigate('Feed') }}
-        >
-          <View style={{ justifyContent: 'center', flexWrap: 'wrap', marginHorizontal: 10 }}>
-            <Text style={{ fontSize: 22 }}>My relatives/friends</Text>
+    return (
+      <View style={styles.container}>
+
+        {/* THIS IS THE USER INFO */}
+        <View style={styles.user}>
+
+          <View style={{ alignItems: 'flex-end', justifyContent: 'flex-end', padding: '1.5%' }}>
+            <Image style={styles.userImage} resizeMode='contain' source={require('../assets/JohnDoe.png')} />
           </View>
 
-          <View style={{ width: 35, height: 35, marginHorizontal: 10 }}>
-            <Image style={{ width: '100%', height: '100%' }} source={require('../assets/Vector.png')} />
+
+          <View style={styles.userInfo}>
+            <View style={styles.userContainer}>
+              <Text style={styles.userName}>John Doe</Text>
+            </View>
+
+            <View style={styles.userContainer}>
+              <Text style={styles.userNo}>NHS NUMBER: 000 000 0000</Text>
+            </View>
+
           </View>
-        </TouchableOpacity>
+
+        </View>
+        {/* THIS IS THE USER INFO */}
+
+        {/* THIS IS THE BUTTON */}
+        <View style={{ padding: '2%' }}>
+          <TouchableOpacity style={styles.button} onPress={() => setModalOpen(true)}>
+            <View style={styles.buttonTitle}>
+              <Text style={styles.buttonText}>My relatives/friends</Text>
+            </View>
+
+            <View style={styles.buttonImage}>
+              <Image style={styles.buttonAdd} resizeMode='contain' source={require('../assets/Vector.png')} />
+            </View>
+
+
+            <Modal transparent={true} visible={modalOpen} animationType='fade'>
+              <View style={styles.popupBack}>
+                <View style={styles.popup}>
+
+                  <View style={styles.popupText}>
+                    <Text style={{ fontFamily: 'Bold', fontSize: 23 }}>New Patient</Text>
+                  </View>
+
+                  <View style={styles.popupText}>
+                    <Text style={{ fontFamily: 'Regular', fontSize: 18 }}>NHS Number:</Text>
+                    <TextInput style={styles.popupInput} minLength={10} maxLength={10}></TextInput>
+                  </View>
+
+                  <View style={styles.notify}>
+                    <Text style={{ fontFamily: 'Regular', fontSize: 15 }}>Notify me if my relative{"\n"}           passes away</Text>
+                    <CheckBox />
+                  </View>
+
+                  <View style={styles.popupButtons}>
+                    <TouchableOpacity onPress={() => setModalOpen(false)}>
+                      <View style={styles.popupAdd}>
+                        <Text style={{ fontFamily: 'Medium', fontSize: 16, color: 'white' }}>Add</Text>
+                      </View>
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity onPress={() => setModalOpen(false)}>
+                      <View style={styles.popupCancel}>
+                        <Text style={{ fontFamily: 'Medium', fontSize: 16, color: 'white' }}>Cancel</Text>
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+
+                </View>
+              </View>
+            </Modal>
+
+          </TouchableOpacity>
+
+        </View>
+
+        {/* THIS IS THE USER BUTTON */}
+
+
+        {/* THIS IS THE USER RELATIVE CODE */}
+        <View>
+          <TouchableOpacity style={styles.relativeCards} onPress={() => navigation.navigate('Feed')}
+
+          >
+
+            <View style={styles.relative}>
+              <Text style={styles.relativeName}>Jane Doe</Text>
+            </View>
+
+
+            <View style={styles.relativeIcon}>
+              <Image style={styles.relatveImage} source={require('../assets/Vectorperson.png')} />
+            </View>
+
+
+            <View>
+              <Text style={styles.relativeHospital}>St.George&#39;s Hospital{"\n"}Last updated: 14th June 10:20</Text>
+            </View>
+
+          </TouchableOpacity>
+
+          <View style={styles.relativeCards}>
+            <View style={styles.relative}>
+              <Text style={styles.relativeName}>Jill Doe</Text>
+            </View>
+
+            <View style={styles.relativeIcon}>
+              <Image style={styles.relatveImage} source={require('../assets/Vectorperson.png')} />
+            </View>
+
+            <View>
+              <Text style={styles.relativeHospital}>St.George&#39;s Hospital{"\n"}Last updated: 18th June 20:00</Text>
+            </View>
+
+          </View>
+        </View>
+        {/* THIS IS THE USER RELATIVE CODE */}
 
       </View>
-      {/* THIS IS THE USER BUTTON */}
-
-
-      {/* THIS IS THE USER RELATIVE CODE */}
-      <TouchableOpacity style={{ marginLeft: '10%', justifyContent: 'space-between', flexWrap: 'wrap', alignItems: 'flex-end', flexDirection: 'row', backgroundColor: '#58ACA8', padding: '2%', marginVertical: '2%' }}>
-
-        <View style={{ flexWrap: 'wrap', height: 100 }}>
-          <Text style={{ fontSize: 25, color: 'white', paddingBottom: '3%' }}>Jane Doe</Text>
-          <Text style={{ fontSize: 18, color: 'white', lineHeight: 25 }}>St.George&#39;s Hospital{"\n"}Last updated: 14th June 10:20</Text>
-        </View>
-
-        <View style={{ width: 75, height: 75, marginBottom: '7%', marginRight: '1%' }}>
-          <Image style={{ width: 60, height: 60 }} source={require('../assets/Vectorperson.png')} />
-        </View>
-      </TouchableOpacity>
-
-      <View style={{ marginLeft: '10%', justifyContent: 'space-between', flexWrap: 'wrap', alignItems: 'flex-end', flexDirection: 'row', backgroundColor: '#58ACA8', padding: '2%', marginVertical: '2%' }}>
-
-        <View style={{ flexWrap: 'wrap', height: 100 }}>
-          <Text style={{ fontSize: 25, color: 'white', paddingBottom: '3%' }}>Jill Doe</Text>
-          <Text style={{ fontSize: 18, color: 'white', lineHeight: 25 }}>St.George&#39;s Hospital{"\n"}Last updated: 18th June 20:00</Text>
-        </View>
-
-        <View style={{ width: 75, height: 75, marginBottom: '7%', marginRight: '1%' }}>
-          <Image style={{ width: 60, height: 60 }} source={require('../assets/Vectorperson.png')} />
-        </View>
-      </View>
-      {/* THIS IS THE USER RELATIVE CODE */}
-
-    </View>
-  );
+    );
+  }
 }
+
 export default Contacts;
 
 const styles = StyleSheet.create({
@@ -112,4 +171,171 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     backgroundColor: '#fff',
   },
-});  
+
+  user: {
+    flexDirection: 'column',
+    padding: 20,
+    justifyContent: 'flex-end',
+    paddingBottom: '0.5%'
+  },
+  userImage: {
+    width: 150,
+    height: 150,
+  },
+  userInfo: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  userContainer: {
+    flexWrap: 'wrap',
+    marginRight: '5%'
+  },
+  userName: {
+    fontSize: 18,
+    fontFamily: 'Bold'
+  },
+  userNo: {
+    fontSize: 16,
+    fontFamily: 'Regular'
+  },
+  button: {
+    marginRight: '15%',
+    marginLeft: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    padding: 10,
+    shadowColor: 'rgba(0,0,0, .25)',
+    shadowOffset: { height: 0, width: 0 },
+    shadowOpacity: 2,
+    shadowRadius: 4,
+    elevation: 5,
+    borderRadius: 40
+  },
+  buttonTitle: {
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    marginHorizontal: 10
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Bold'
+  },
+  buttonImage: {
+    width: 35,
+    height: 35,
+    marginHorizontal: 10
+  },
+  buttonAdd: {
+    width: '100%',
+    height: '100%'
+  },
+  popupBack: {
+    flex: 1,
+    backgroundColor: "#000000aa",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  popup: {
+    borderRadius: 40,
+    height: '50%',
+    width: '75%',
+    padding: '5%',
+    backgroundColor: 'white'
+  },
+
+  popupText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: '2%'
+  },
+
+  popupInput: {
+    height: 30,
+    width: '90%',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    marginVertical: '5%'
+  },
+
+  notify: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: "row",
+    marginTop: '20%'
+  },
+
+  popupButtons: {
+    height: '20%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: "row"
+  },
+
+
+  popupAdd: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#58ACA8',
+    padding: 5,
+    paddingLeft: 30,
+    paddingRight: 30,
+    borderRadius: 40
+  },
+
+  popupCancel: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#AC5858',
+    width: '100%',
+    padding: 5,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 40
+  },
+
+  relativeCards: {
+    marginLeft: '10%',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    backgroundColor: '#58ACA8',
+    padding: '2%',
+    marginVertical: '2%'
+  },
+
+  relative: {
+    flexWrap: 'wrap',
+    paddingBottom: '5%'
+  },
+
+  relativeName: {
+    fontFamily: 'Medium',
+    fontSize: 22,
+    color: 'white'
+  },
+
+  relativeIcon: {
+    width: 75,
+    height: 75,
+    marginRight: '5%'
+  },
+
+  relativeImage: {
+    width: 75,
+    height: 75
+  },
+
+  relativeHospital: {
+    fontFamily: 'Regular',
+    fontSize: 18,
+    color: 'white',
+    lineHeight: 30
+  },
+
+});
