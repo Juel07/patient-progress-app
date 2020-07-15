e5b2a6cf43936d6ad12f1ccb8ca412313213d7f5
kate2797 <katka.zaprazna@icloud.com>
2020-07-15 18:07:40 +0200

    Code formattin

--- Components/TimelineInfo.js
+++ Components/TimelineInfo.js
@@ -1,18 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
-import OnLayout from 'react-native-on-layout';
-
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Image, Dimensions } from "react-native";
+import OnLayout from "react-native-on-layout";
 
 const TimelineInfo = (props) => {
-
     return (
         <>
-
             {/* Parent View*/}
             <View style={styles.parent}>
-
                 {/* Time */}
-                <Text style={styles.time}>{props.rawData.health_recordings[0].date_modified.slice(11, 16)}</Text>
+                <Text style={styles.time}>
+                    {props.rawData.health_recordings[0].date_modified.slice(11, 16)}
+                </Text>
 
                 {/* Data wrapper for Timeline & Data */}
                 <View style={styles.parentWrap}>
@@ -23,18 +21,22 @@ const TimelineInfo = (props) => {
                     </View>
                     {/* API Data */}
                     <View>
-                        <Text style={styles.date}>{props.rawData.health_recordings[0].date_modified.slice(8, 10)}/{props.rawData.health_recordings[0].date_modified.slice(5, 7)}/{props.rawData.health_recordings[0].date_modified.slice(0, 4)}</Text>
+                        <Text style={styles.date}>
+                            {props.rawData.health_recordings[0].date_modified.slice(8, 10)}/
+                            {props.rawData.health_recordings[0].date_modified.slice(5, 7)}/
+                            {props.rawData.health_recordings[0].date_modified.slice(0, 4)}
+                        </Text>
 
-                        <View style={{ flexDirection: 'row' }}>
-                            <Text style={styles.description} numberOfLines={4} ellipsizeMode="tail">{props.description}{props.data}{props.extras}.</Text>
+                        <View style={{ flexDirection: "row" }}>
+                            <Text style={styles.description} numberOfLines={4} ellipsizeMode="tail">
+                                {props.description}
+                                {props.data}
+                                {props.extras}.
+                            </Text>
                         </View>
-
-
                     </View>
                 </View>
-
             </View>
-
         </>
     );
 };
@@ -44,48 +46,48 @@ export default TimelineInfo;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        backgroundColor: '#ecf0f1',
+        justifyContent: "center",
+        backgroundColor: "#ecf0f1",
         padding: 8,
     },
     circle: {
-        marginTop: '1.14%',
+        marginTop: "1.14%",
         width: 16,
         height: 16,
         borderRadius: 100 / 2,
-        backgroundColor: '#58ACA8',
+        backgroundColor: "#58ACA8",
         zIndex: 1,
     },
     rectangle: {
         marginLeft: 7,
         width: 2,
         height: 14 * 4,
-        backgroundColor: '#58ACA8',
+        backgroundColor: "#58ACA8",
         zIndex: 0,
         marginTop: -2,
     },
     parent: {
-        display: 'flex',
-        flexDirection: 'row',
+        display: "flex",
+        flexDirection: "row",
         marginLeft: 22,
     },
     parentWrap: {
-        display: 'flex',
-        flexDirection: 'row',
+        display: "flex",
+        flexDirection: "row",
     },
     date: {
-        color: 'black',
+        color: "black",
         fontSize: 16,
-        fontWeight: 'bold',
+        fontWeight: "bold",
     },
     time: {
         fontSize: 14,
         marginRight: 15,
     },
     description: {
-        color: 'black',
+        color: "black",
         fontSize: 16,
-        fontFamily: 'Regular',
+        fontFamily: "Regular",
         paddingTop: 6,
         width: 259,
     },
