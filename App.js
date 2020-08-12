e875fc7f914968bd1199f61052c88c67a51a7cef
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-08-12 22:28:19 +0100

    clean up

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import RootStackScreen from './Components/RootStackScreen';
-import MainTabScreen from './Components/MainTabScreen';
 
 const App = () =>{
   return(
     <NavigationContainer>
       <RootStackScreen />
-      {/* <MainTabScreen/> */}
     </NavigationContainer>
   );
 }
