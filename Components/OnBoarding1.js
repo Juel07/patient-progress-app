194ec758199ed60ccad52f6c5c54c6fec73cdd42
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-08-13 01:13:45 +0100

    button style + formatted

--- Components/OnBoarding1.js
+++ Components/OnBoarding1.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import { Image, StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
-import logo from '../assets/logo.png';
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import logo from "../assets/logo.png";
 
-// importing fonts 
-import { useFonts } from '@use-expo/font';
-import { AppLoading } from 'expo';
+// importing fonts
+import { useFonts } from "@use-expo/font";
+import { AppLoading } from "expo";
 
 export default function OnBoardingOne({ navigation }) {
-
   const pressHandler = () => {
-    navigation.navigate('Welcome-2')
-  }
+    navigation.navigate("Welcome-2");
+  };
 
   // fonts load
   let [fontsLoaded] = useFonts({
-    'Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
-    'Bold': require('../assets/fonts/Montserrat-SemiBold.ttf')
+    Regular: require("../assets/fonts/Montserrat-Regular.ttf"),
+    Bold: require("../assets/fonts/Montserrat-SemiBold.ttf"),
   });
 
   // check if fonts are loaded
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
-
     return (
-
       <View style={styles.container}>
-        <StatusBar backgroundColor='#32afa9' barStyle="light-content" />
+        <StatusBar backgroundColor="#32afa9" barStyle="light-content" />
         <Image source={logo} style={{ width: 100, marginBottom: 10 }} />
         <Text style={styles.headings}>Welcome</Text>
-        <Text style={styles.textBox}>We know it's a difficult time now and the healthcare staff are busy. With Patient Progress, you can keep track of how your relatives are doing.</Text>
+        <Text style={styles.textBox}>
+          We know it's a difficult time now and the healthcare staff are busy.
+          With Patient Progress, you can keep track of how your relatives are
+          doing.
+        </Text>
 
         <View style={styles.dots}>
           <View style={styles.circleFirst}></View>
@@ -38,47 +47,68 @@ export default function OnBoardingOne({ navigation }) {
         </View>
 
         <View style={styles.btn}>
-          <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center' }} onPress={pressHandler}>
-            <Text style={{ fontSize: 18, fontFamily: 'Bold', color: '#58ACA8' }}>Next</Text>
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: 30,
+              borderColor: "#58ACA8",
+              borderWidth: 2,
+              borderRadius: 10,
+              padding: 10,
+            }}
+            onPress={pressHandler}
+          >
+            <Text
+              style={{ fontSize: 18, fontFamily: "Bold", color: "#58ACA8" }}
+            >
+              Next
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center' }} onPress={() => navigation.navigate('MainScreen')}>
-            <Text style={{ fontSize: 18, fontFamily: 'Bold', color: '#58ACA8' }}>Skip</Text>
+          <TouchableOpacity
+            style={{ alignItems: "center", justifyContent: "center" }}
+            onPress={() => navigation.navigate("MainScreen")}
+          >
+            <Text
+              style={{ fontSize: 18, fontFamily: "Bold", color: "#58ACA8" }}
+            >
+              Skip
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
     );
   }
-};
-
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
     paddingLeft: 37,
     paddingRight: 38,
   },
 
   textBox: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 18,
-    fontFamily: 'Regular'
+    fontFamily: "Regular",
   },
 
   headings: {
     fontSize: 23,
     paddingBottom: 6,
-    fontFamily: 'Bold'
+    fontFamily: "Bold",
   },
 
   circle: {
     width: 9,
     height: 9,
     borderRadius: 100 / 2,
-    backgroundColor: '#C4C4C4',
+    backgroundColor: "#C4C4C4",
     margin: 3,
     marginTop: 45,
   },
@@ -87,18 +117,19 @@ const styles = StyleSheet.create({
     width: 9,
     height: 9,
     borderRadius: 100 / 2,
-    backgroundColor: '#58ACA8',
+    backgroundColor: "#58ACA8",
     margin: 3,
     marginTop: 45,
   },
 
   dots: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
 
   btn: {
-    marginTop: 20
-  }
-
-
+    marginTop: 20,
+  },
 });
+
+const { width } = Dimensions.get("screen");
+const button_width = width * 100;
--- Components/OnBoarding2.js
+++ Components/OnBoarding2.js
@@ -1,35 +1,43 @@
-import React from 'react';
-import { Image, StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
-import logo from '../assets/logo.png';
+import React from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
+import logo from "../assets/logo.png";
 
-// importing fonts 
-import { useFonts } from '@use-expo/font';
-import { AppLoading } from 'expo';
+// importing fonts
+import { useFonts } from "@use-expo/font";
+import { AppLoading } from "expo";
 
 export default function OnBoardingTwo({ navigation }) {
-
   const pressHandler2 = () => {
-    navigation.navigate('Welcome-3')
-  }
+    navigation.navigate("Welcome-3");
+  };
 
   // fonts load
   let [fontsLoaded] = useFonts({
-    'Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
-    'Bold': require('../assets/fonts/Montserrat-SemiBold.ttf')
+    Regular: require("../assets/fonts/Montserrat-Regular.ttf"),
+    Bold: require("../assets/fonts/Montserrat-SemiBold.ttf"),
   });
 
   // check if fonts are loaded
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
-
     return (
-
       <View style={styles.container}>
-        <StatusBar backgroundColor='#32afa9' barStyle="light-content" />
+        <StatusBar backgroundColor="#32afa9" barStyle="light-content" />
         <Image source={logo} style={{ width: 100, marginBottom: 10 }} />
         <Text style={styles.headings}>Track progress</Text>
-        <Text style={styles.textBox}>You can add your loved ones and see how they are doing in real time. You will receive a notification everytime there's a new update in the Feed.</Text>
+        <Text style={styles.textBox}>
+          You can add your loved ones and see how they are doing in real time.
+          You will receive a notification everytime there's a new update in the
+          Feed.
+        </Text>
 
         <View style={styles.dots}>
           <View style={styles.circle}></View>
@@ -38,46 +46,68 @@ export default function OnBoardingTwo({ navigation }) {
         </View>
 
         <View style={styles.btn}>
-          <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center' }} onPress={pressHandler2}>
-            <Text style={{ fontSize: 18, fontFamily: 'Bold', color: '#58ACA8' }}>Next</Text>
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: 30,
+              borderColor: "#58ACA8",
+              borderWidth: 2,
+              borderRadius: 10,
+              padding: 10,
+            }}
+            onPress={pressHandler2}
+          >
+            <Text
+              style={{ fontSize: 18, fontFamily: "Bold", color: "#58ACA8" }}
+            >
+              Next
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center' }} onPress={() => navigation.goBack()}>
-            <Text style={{ fontSize: 18, fontFamily: 'Bold', color: '#58ACA8' }}>Previous</Text>
+          <TouchableOpacity
+            style={{ alignItems: "center", justifyContent: "center" }}
+            onPress={() => navigation.goBack()}
+          >
+            <Text
+              style={{ fontSize: 18, fontFamily: "Bold", color: "#58ACA8" }}
+            >
+              Previous
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
     paddingLeft: 37,
     paddingRight: 38,
   },
 
   textBox: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 18,
-    fontFamily: 'Regular'
+    fontFamily: "Regular",
   },
 
   headings: {
     fontSize: 23,
     paddingBottom: 6,
-    fontFamily: 'Bold'
+    fontFamily: "Bold",
   },
 
   circle: {
     width: 9,
     height: 9,
     borderRadius: 100 / 2,
-    backgroundColor: '#C4C4C4',
+    backgroundColor: "#C4C4C4",
     margin: 3,
     marginTop: 45,
   },
@@ -86,18 +116,16 @@ const styles = StyleSheet.create({
     width: 9,
     height: 9,
     borderRadius: 100 / 2,
-    backgroundColor: '#58ACA8',
+    backgroundColor: "#58ACA8",
     margin: 3,
     marginTop: 45,
   },
 
   dots: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
 
   btn: {
-    marginTop: 20
-  }
-
-
+    marginTop: 20,
+  },
 });
